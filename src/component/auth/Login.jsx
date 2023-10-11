af4b6701e5defb45fae17ca55e65768afe45dd5f
ibrataha8 <ibrataha8@gmail.com>
2023-10-11 17:01:39 +0100

    save token in localstoragr

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -7,7 +7,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import axios from "axios";
 function Copyright(props) {
   return (
     <Typography
@@ -24,17 +24,21 @@ function Copyright(props) {
   );
 }
 
-
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
+    const response = await axios.post("http://127.0.0.1:3000/login", {
+      login: data.get("email"),
       password: data.get("password"),
     });
+    const token = response.data.token;
+    if (token) {
+      localStorage.setItem("token", token);
+      
+    }
   };
 
   return (
